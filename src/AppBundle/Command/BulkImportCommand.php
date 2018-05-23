09403d2b321b996e2082696a248bd6eaded8f14a
HamHamFonFon <balistik.fonfon@gmail.com>
2018-05-23 20:43:17 +0200

    Change mapping and data

---
+++ src/AppBundle/Command/BulkImportCommand.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace AppBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class BulkImportCommand extends ContainerAwareCommand
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this->setName('dso:import:bulk')->setDescription('Hello PhpStorm');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+
+        $dataFile =  $this->getContainer()->get('kernel')->getRootDir() . DIRECTORY_SEPARATOR . 'Resources/data/dso6.bulk.json';
+
+        $contentFile = file_get_contents($dataFile);
+        $newData = preg_replace_callback('/%randId%/', function() {
+            return md5(uniqid());
+        }, $contentFile);
+
+        $dataNewFile =  $this->getContainer()->get('kernel')->getRootDir() . DIRECTORY_SEPARATOR . 'Resources/data/dso.generate.data.json';
+        file_put_contents($dataNewFile, $newData);
+    }
+}
