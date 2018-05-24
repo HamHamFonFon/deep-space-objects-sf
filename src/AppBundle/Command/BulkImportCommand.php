49171716c18461a5ad3a3055a216c28cbef4f26d
HamHamFonFon <balistik.fonfon@gmail.com>
2018-05-24 22:27:57 +0200

    Test import bulk

--- src/AppBundle/Command/BulkImportCommand.php
+++ src/AppBundle/Command/BulkImportCommand.php
@@ -25,11 +25,16 @@ class BulkImportCommand extends ContainerAwareCommand
         $dataFile =  $this->getContainer()->get('kernel')->getRootDir() . DIRECTORY_SEPARATOR . 'Resources/data/dso6.bulk.json';
 
         $contentFile = file_get_contents($dataFile);
+
+        // Change ID
         $newData = preg_replace_callback('/%randId%/', function() {
             $datetime = new \DateTime();
             $prefix = $datetime->getTimestamp();
             return md5(uniqid($prefix));
         }, $contentFile);
+
+        // Change Catalog
+        $mappingCat = ['NGC' => 'ngc', 'IC' => 'ic'];
 
         $dataNewFile =  $this->getContainer()->get('kernel')->getRootDir() . DIRECTORY_SEPARATOR . 'Resources/data/dso.generate.data.json';
         file_put_contents($dataNewFile, $newData);
