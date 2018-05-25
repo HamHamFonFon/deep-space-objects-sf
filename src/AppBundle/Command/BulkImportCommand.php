8407c27b58e664bd9ee4abaeda0edcfbf79ab950
HamHamFonFon <balistik.fonfon@gmail.com>
2018-05-26 00:44:49 +0200

    Data full DSO

--- src/AppBundle/Command/BulkImportCommand.php
+++ src/AppBundle/Command/BulkImportCommand.php
@@ -17,48 +17,34 @@ class BulkImportCommand extends ContainerAwareCommand
         'St' => 'sto',
         'Ab' => 'abl',
         'UG' => 'ugc',
-        'Ap' => '',
+        'An' => '', 'Ap' => '', 'AP' => '',
         'He' => '',
-        'Ba' => '',
-        'Be' => '',
-        'Bi' => '',
-        'Bo' => '',
-        'B1' => '',
-        'B2' => '',
-        'B3' => '',
-        'B8' => '',
-        'B9' => '',
-        'K1' => '',
-        'K2' => '',
-        'K3' => '',
-        'M1' => '',
-        'M2' => '',
-        'M3' => '',
-        'M4' => '',
-        'AP' => '',
+        'Ba' => '', 'Be' => '', 'Bi' => '', 'Bo' => '',
+        'B1' => '', 'B2' => '', 'B3' => '', 'B4' => '', 'B5' => '', 'B6' => '', 'B7' => '', 'B8' => '', 'B9' => '',
+        'K1' => '', 'K2' => '', 'K3' => '', 'K4' => '',
+        'M1' => '', 'M2' => '', 'M3' => '', 'M4' => '', 'M7' => '',
         'Cz' => '',
         'Ki' => '',
         'Do' => '',
-        'Pa' => '',
-        'Pe' => '',
+        'Pa' => '', 'Pe' => '',
         'Ce' => '',
         'Ru' => '',
         'Ly' => '',
-        'Ha' => '',
-        'Ho' => '',
-        'Hu' => '',
+        'Ha' => '', 'Ho' => '', 'Hu' => '',
+        'H1' => '', 'H2' => '',
         'vd' => '',
         'Ca' => '',
         'La' => '',
         'Me' => '',
         '3C' => '',
-        'To' => '',
-        'Tr' => '',
-        'Gu' => '',
-        'Gr' => '',
+        'Te' => '', 'To' => '', 'Tr' => '',
+        'Gu' => '', 'Gr' => '',
         'Pi' => '',
         'Fe' => '',
-        'Ro' => ''
+        'Ro' => '',
+        'Jo' => '',
+        'J3' => '', 'J9' => '',
+        'Vd' => '', 'VV' => ''
 
     ];
 
@@ -102,21 +88,28 @@ class BulkImportCommand extends ContainerAwareCommand
      */
     private function transformCatalog()
     {
-        $file = $this->getContainer()->get('kernel')->getRootDir() . DIRECTORY_SEPARATOR . 'Resources/data/dso20.bulk.generate.json';
-        $jsonFile = json_decode(file_get_contents($file), true);
-
         $mapping = self::$mapping;
-        foreach ($jsonFile['bulkData'] as $dso) {
+        $file = $this->getContainer()->get('kernel')->getRootDir() . DIRECTORY_SEPARATOR . 'Resources/data/dso20.bulk.generate.json';
+        $jsonData = json_decode(file_get_contents($file), true);
+
+        foreach ($jsonData['bulkData'] as $key=>$dso) {
             if (preg_match('/%catalog%/', json_encode($dso))) {
                 $type = substr($dso['id'], 0, 2);
                 $valueReplace = $mapping[$type];
-                $dso = preg_replace_callback('/%catalog%/', function() use ($valueReplace){
+
+                $dsoStr = json_encode($dso);
+
+                $newValueCatalog = preg_replace_callback('/%catalog%/', function() use ($valueReplace){
                     return $valueReplace;
-                }, json_encode($dso));
+                }, $dsoStr);
+
+                $jsonData['bulkData'][$key] = json_decode($newValueCatalog);
+//                dump($newValueCatalog) . "\n";
             }
         }
 
-        dump($jsonFile['bulkData']);
+        $dataCleanFile =  $this->getContainer()->get('kernel')->getRootDir() . DIRECTORY_SEPARATOR . 'Resources/data/dso20.bulk.generate.json';
+        file_put_contents($dataCleanFile, json_encode($jsonData, JSON_UNESCAPED_SLASHES));
         return;
     }
 }
