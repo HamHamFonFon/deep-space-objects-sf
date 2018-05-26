c2fa220e4941b735c9ccd9a5be63b3b0e387da7e
HamHamFonFon <balistik.fonfon@gmail.com>
2018-05-26 20:49:51 +0200

    Fix data DSO and script import data

--- src/AppBundle/Command/BulkImportCommand.php
+++ src/AppBundle/Command/BulkImportCommand.php
@@ -2,6 +2,8 @@
 
 namespace AppBundle\Command;
 
+use AppBundle\Repository\DsoRepository;
+use Kuzzle\Kuzzle;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -45,8 +47,14 @@ class BulkImportCommand extends ContainerAwareCommand
         'Jo' => '',
         'J3' => '', 'J9' => '',
         'Vd' => '', 'VV' => ''
-
     ];
+
+    private $kernel;
+    private $srcFile = 'Resources/data/Bulks/dso20.bulk.src.json';
+    private $generatedFile = 'Resources/data/Bulks/dso20.bulk.generate.json';
+    protected $kuzzle;
+    private $kuzzleIndex;
+    protected $listErrors = [];
 
     /**
      * {@inheritdoc}
@@ -61,24 +69,38 @@ class BulkImportCommand extends ContainerAwareCommand
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $this->transformId();
+        // Files
+        $this->kernel = $kernel = $this->getContainer()->get('kernel')->getRootDir() . DIRECTORY_SEPARATOR;
+        $this->srcFile = $kernel . $this->srcFile;
+        $this->generatedFile = $kernel . $this->generatedFile;
+
+        // Init Kuzzle
+        $kuzzleHost = $this->getContainer()->getParameter('kuzzle_host');
+        $this->kuzzleIndex = $this->getContainer()->getParameter('kuzzle_index');
+        $kuzzlePort = $this->getContainer()->getParameter('kuzzle_port');
+
+        /** @var Kuzzle kuzzle */
+        $this->kuzzle = new Kuzzle($kuzzleHost, [
+            'defaultIndex' => $this->kuzzleIndex,
+            'port' => $kuzzlePort
+        ]);
+
+//        $this->transformId();
         $this->transformCatalog();
+        $output->writeln('File errors');
+        $output->writeln($this->listErrors);
     }
 
     private function transformId()
     {
-        $dataFile =  $this->getContainer()->get('kernel')->getRootDir() . DIRECTORY_SEPARATOR . 'Resources/data/dso20.bulk.json';
-        $contentFile = file_get_contents($dataFile);
+        $contentFile = file_get_contents($this->srcFile);
 
         // Change ID
         $newData = preg_replace_callback('/%randId%/', function() {
-            $datetime = new \DateTime();
-            $prefix = $datetime->getTimestamp();
-            return md5(uniqid($prefix));
+            return $this->generateKuzzleId();
         }, $contentFile);
 
-        $dataNewFile =  $this->getContainer()->get('kernel')->getRootDir() . DIRECTORY_SEPARATOR . 'Resources/data/dso20.bulk.generate.json';
-        file_put_contents($dataNewFile, $newData);
+        file_put_contents($this->generatedFile, $newData);
 
         return;
     }
@@ -89,27 +111,61 @@ class BulkImportCommand extends ContainerAwareCommand
     private function transformCatalog()
     {
         $mapping = self::$mapping;
-        $file = $this->getContainer()->get('kernel')->getRootDir() . DIRECTORY_SEPARATOR . 'Resources/data/dso20.bulk.generate.json';
-        $jsonData = json_decode(file_get_contents($file), true);
+        $jsonData = json_decode(file_get_contents($this->srcFile), true);
 
         foreach ($jsonData['bulkData'] as $key=>$dso) {
             if (preg_match('/%catalog%/', json_encode($dso))) {
+
                 $type = substr($dso['id'], 0, 2);
                 $valueReplace = $mapping[$type];
 
                 $dsoStr = json_encode($dso);
-
                 $newValueCatalog = preg_replace_callback('/%catalog%/', function() use ($valueReplace){
                     return $valueReplace;
                 }, $dsoStr);
 
-                $jsonData['bulkData'][$key] = json_decode($newValueCatalog);
-//                dump($newValueCatalog) . "\n";
+//                $jsonData['bulkData'][$key] = json_decode($newValueCatalog);
+                $document = json_decode($newValueCatalog, true);
+                $id = $this->generateKuzzleId();
+
+               $this->insertDso($id, $document);
             }
         }
 
-        $dataCleanFile =  $this->getContainer()->get('kernel')->getRootDir() . DIRECTORY_SEPARATOR . 'Resources/data/dso20.bulk.generate.json';
-        file_put_contents($dataCleanFile, json_encode($jsonData, JSON_UNESCAPED_SLASHES));
+//        $dataCleanFile =  $this->getContainer()->get('kernel')->getRootDir() . DIRECTORY_SEPARATOR . 'Resources/data/dso20.bulk.generate.json';
+//        file_put_contents($dataCleanFile, json_encode($jsonData, JSON_UNESCAPED_SLASHES));
         return;
+    }
+
+
+    /**
+     * Insert data in Kuzzle
+     * @param $id
+     * @param $document
+     */
+    private function insertDso($id, $document)
+    {
+        $kuzzleCollection = $this->kuzzle->collection(DsoRepository::COLLECTION_NAME, $this->kuzzleIndex);
+        try {
+//            dump("Create document $id [" . $document['id'] ."]");
+            $kuzzleCollection->createDocument($document, $id);
+        } catch(\ErrorException $e) {
+            $this->listErrors[] = '[' . $document['id'] . ']' . $e->getMessage();
+        } catch (\Exception $e) {
+            $this->listErrors[] = '[' . $document['id'] . ']' . $e->getMessage();
+        }
+
+        return;
+    }
+
+    /**
+     * Generate unique ID
+     * @return string
+     */
+    private function generateKuzzleId()
+    {
+        $datetime = new \DateTime();
+        $prefix = $datetime->getTimestamp();
+        return md5(uniqid($prefix));
     }
 }
